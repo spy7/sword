26f58b36ae92e1a1af8eb2c4b62ed97f26e20446
Carlos Souza <carlosspy@gmail.com>
2024-09-25 10:37:23 +0100

    feat: create be-sword

---
+++ README.md
@@ -0,0 +1,42 @@
+# Sword Health 👋
+
+[![Generic badge](https://img.shields.io/badge/python-3.12-yellow.svg)]()
+[![Generic badge](https://img.shields.io/badge/django-3.2-yellow.svg)]()
+
+> Challenge for Sword Health
+
+## Conteúdo
+
+Há um projeto nesse desafio:
+
+* be_sword: Backend em Django
+
+### BE_SWORD
+
+```
+be_sword
+├── be_sword
+├── book
+│   └── migrations
+```
+
+## 🎯 Pré requisitos
+
+Para iniciar a plataforma, precisa-se inicialmente do python 3.12 instalado o seu ambiente virtual de preferência,
+caso queira prosseguir com a instalação local.
+Para execução, recomenda-se a instalação do Docker.
+
+* [Instalação do Python](https://www.python.org/downloads/)
+* [Instalação do Docker](https://docs.docker.com/engine/install/ubuntu/)
+
+## 🚗 Execução
+
+Assumindo que você tem o docker instalado
+
+```bash
+docker compose up
+```
+
+## 📚 Documentação da api
+
+No formato [swagger](http://127.0.0.1:8000/swagger)
---
+++ be_sword/book/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Book(models.Model):
+    goodreads_book_id = models.IntegerField()
+    best_book_id = models.IntegerField()
+    work_id = models.IntegerField()
+    books_count = models.IntegerField()
+    isbn = models.CharField(max_length=15)
+    isbn13 = models.CharField(max_length=15)
+    authors = models.CharField(max_length=255)
+    original_publication_year = models.IntegerField()
+    original_title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    language_code = models.CharField(max_length=10)
+    average_rating = models.FloatField()
+    ratings_count = models.IntegerField()
+    work_ratings_count = models.IntegerField()
+    work_text_reviews_count = models.IntegerField()
+    ratings_1 = models.IntegerField()
+    ratings_2 = models.IntegerField()
+    ratings_3 = models.IntegerField()
+    ratings_4 = models.IntegerField()
+    ratings_5 = models.IntegerField()
+    image_url = models.URLField()
+    small_image_url = models.URLField()
+
+    def __str__(self):
+        return self.title
